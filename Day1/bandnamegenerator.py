99be60469594f28b19ff3d9aa8193961358fd4ef
Kalan <bkalan32@gmail.com>
2023-11-08 16:34:24 -0500

    Day 1 Band name generator

---
+++ Day1/bandnamegenerator.py
@@ -0,0 +1,11 @@
+#1 Create a greeting for Band Name generator
+print("Welcome to Big K's band name generator")
+
+# Ask the user for the city they group in
+city = input("What city did you grow up in?\n")
+city
+# Ask the user for name of their favorite food.
+food = input("What's your favorite food?\n")
+food
+# Combine the name of the their city and food and show their band name.
+print("Your band name is", city + " " + food)
